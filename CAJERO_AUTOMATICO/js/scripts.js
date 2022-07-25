2113a342347575fb93952db0788e40741af88e78
Jesus Manuel Juarez Trejo <jesus.juarezt@coppel.com>
2022-07-25 17:54:17 -0600

    Se sube front de Login

---
+++ CAJERO_AUTOMATICO/js/scripts.js
@@ -0,0 +1,19 @@
+var cuentas = [
+    { 
+        id: 1,
+        nombre:'Mali', 
+        contrasena:'M@l¡',
+        saldo: 200 
+    },
+    { 
+        id: 2,
+        nombre:'Gera', 
+        contrasena:'gera_007!',
+        saldo: 290 },
+    { 
+        id:3,
+        nombre:'Maui', 
+        contrasena:'maui123',
+        saldo: 67 
+    }
+  ];
